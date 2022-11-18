aa42f18893b8a93523f5035186ffe8e7bce8ebde
nelllyai <worldpiece000@gmail.com>
2022-11-18 22:32:48 +0300

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+const product = "Колесо велосипедное переднее";
+const quantity = 8;
+const category = "Запчасти";
+const price = 1450;
+
+console.log("Наименование товара:", product);
+console.log("Общая сумма товара:", quantity * price);
