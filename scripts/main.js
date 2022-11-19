7a25407d8ca4704c1aba8ddc69750ff8fe092eab
nelllyai <worldpiece000@gmail.com>
2022-11-19 15:38:36 +0300

    m4l3

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,13 @@ const quantity = +prompt("Введите количество:");
 const category = prompt("Укажите категорию товара:");
 const price = +prompt("Уточните цену единицы товара:");
 
-console.log(`Тип данных для наименования товара: ${typeof product}`);
-console.log(`Тип данных для количества: ${typeof quantity}`);
-console.log(`Тип данных для категории: ${typeof category}`);
-console.log(`Тип данных для цены: ${typeof price}`);
-
-console.log(`На складе ${quantity} шт. товара "${product}" на сумму ${quantity * price}`);
+if (!Number.isNaN(quantity) && !Number.isNaN(price)) {
+  console.log(`Тип данных для наименования товара: ${typeof product}`);
+  console.log(`Тип данных для количества: ${typeof quantity}`);
+  console.log(`Тип данных для категории: ${typeof category}`);
+  console.log(`Тип данных для цены: ${typeof price}`);
+  
+  console.log(`На складе ${quantity} шт. товара "${product}" на сумму ${quantity * price}`);
+} else {
+  alert("Вы ввели некорректные данные!");
+}
