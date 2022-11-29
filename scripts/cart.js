b0274c0e93585d0986976421eb46f4b56c134c0c
nelllyai <worldpiece000@gmail.com>
2022-11-29 04:12:20 +0300

    m4l11

--- scripts/cart.js
+++ scripts/cart.js
@@ -3,6 +3,7 @@
 const cart = {
   items: [],
   count: 0,
+  discount: 0,
   get totalPrice() {
     return this.calculateItemPrice();
   },
@@ -20,8 +21,10 @@ const cart = {
     this.count += num;
   },
   calculateItemPrice() {
-    return this.items.reduce((sum, item) =>
+    const priceWithoutDiscount = this.items.reduce((sum, item) =>
       sum + item.price * item.quantity, 0);
+
+    return priceWithoutDiscount - this.discount / 100 * priceWithoutDiscount;
   },
   clear() {
     this.items.length = 0;
@@ -31,10 +34,25 @@ const cart = {
     console.log(JSON.stringify(this.items));
     console.log(this.totalPrice);
   },
+  set setDiscount(promocode) {
+    if (promocode === 'METHED') {
+      this.discount = 15;
+    } else if (promocode === 'NEWYEAR') {
+      this.discount = 21;
+    }
+  },
 };
 
 cart.add('Ароматические свечи', 800, 5);
 cart.add('Крем для рук', 230, 2);
 cart.add('Масло для волос', 400);
 
+cart.print();
+
+cart.setDiscount = 'METHED';
+console.log('Применили промокод METHED:');
+cart.print();
+
+cart.setDiscount = 'NEWYEAR';
+console.log('Применили промокод NEWYEAR:');
 cart.print();
