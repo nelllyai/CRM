ae2aaa1065d793bb15ec3cbe094486522ba0b30c
nelllyai <worldpiece000@gmail.com>
2022-12-09 21:04:20 +0300

    add form closing

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ const title = modal.querySelector('h2');
 const closeButton = modal.querySelector('.overlay__close-button');
 const productId = modal.querySelector('.form-article__id');
 
-const form = modal.querySelector('form');
+const form = document.querySelector('.form-article');
 
 const discountCheckbox = modal.querySelector('.add-form__checkbox');
 const discountInput = modal
@@ -32,66 +32,21 @@ const renderGoods = (elem, goods) => {
   elem.append(...allRow);
 };
 
-const goods = [
-  {
-    "id": 253842678,
-    "title": "Смартфон Xiaomi 11T 8/128GB",
-    "price": 27000,
-    "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
-    "category": "mobile-phone",
-    "discont": false,
-    "count": 3,
-    "units": "шт",
-    "images": {
-      "small": "img/smrtxiaomi11t-m.jpg",
-      "big": "img/smrtxiaomi11t-b.jpg"
-    }
-  },
-  {
-    "id": 296378448,
-    "title": "Радиоуправляемый автомобиль Cheetan",
-    "price": 4000,
-    "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
-    "category": "toys",
-    "discont": 5,
-    "count": 1,
-    "units": "шт",
-    "images": {
-      "small": "img/cheetancar-m.jpg",
-      "big": "img/cheetancar-b.jpg"
-    }
-  },
-  {
-    "id": 215796548,
-    "title": "ТВ приставка MECOOL KI",
-    "price": 12400,
-    "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
-    "category": "tv-box",
-    "discont": 15,
-    "count": 4,
-    "units": "шт",
-    "images": {
-      "small": "img/tvboxmecool-m.jpg",
-      "big": "img/tvboxmecool-b.jpg"
-    }
-  },
-  {
-    "id": 246258248,
-    "title": "Витая пара PROConnect 01-0043-3-25",
-    "price": 22,
-    "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
-    "category": "cables",
-    "discont": false,
-    "count": 420,
-    "units": "v",
-    "images": {
-      "small": "img/lan_proconnect43-3-25.jpg",
-      "big": "img/lan_proconnect43-3-25-b.jpg"
-    }
-  }
-];
+const overlay = document.querySelector('.overlay');
 
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
-table.append(tbody);
-renderGoods(tbody, goods);
+const addButton = document.querySelector('.cms__add-button');
+addButton.addEventListener('click', () => {
+  overlay.classList.add('overlay_show');
+});
+
+form.addEventListener('click', event => {
+  event.stopPropagation();
+});
+
+overlay.addEventListener('click', () => {
+  overlay.classList.remove('overlay_show');
+});
+
+closeButton.addEventListener('click', () => {
+  overlay.classList.remove('overlay_show');
+});
