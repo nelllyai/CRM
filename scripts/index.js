2520bd972b036e1193e10a75169a4978fb428165
nelllyai <worldpiece000@gmail.com>
2022-12-10 21:29:28 +0300

    fix styles

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ const form = document.querySelector('.form-article');
 
 const discountCheckbox = modal.querySelector('.add-form__checkbox');
 const discountInput = modal
-    .querySelector('.add-form__checkbox ~ .add-form__input');
+  .querySelector('.add-form__checkbox ~ .add-form__input');
 
 const totalPrice = modal.querySelector('.total__price');
 
@@ -39,14 +39,11 @@ addButton.addEventListener('click', () => {
   overlay.classList.add('overlay_show');
 });
 
-form.addEventListener('click', event => {
-  event.stopPropagation();
-});
+overlay.addEventListener('click', event => {
+  const target = event.target;
+  if (target === overlay || target === closeButton) {
+    overlay.classList.remove('overlay_show');
+  }
+})
 
-overlay.addEventListener('click', () => {
-  overlay.classList.remove('overlay_show');
-});
 
-closeButton.addEventListener('click', () => {
-  overlay.classList.remove('overlay_show');
-});
